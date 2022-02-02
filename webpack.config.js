9866925c1598c88840e7c0907c2577c9945a1f6c
art-litv <artem15052003@gmail.com>
2022-02-02 18:43:41 +0200

    fixed everything!!!!

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = (env, argv) => {
     output: {
       path: path.resolve(__dirname, './dist'),
       filename: 'bundle.js',
-      publicPath: '/',
+      publicPath: './',
     },
     module: {
       rules: [
